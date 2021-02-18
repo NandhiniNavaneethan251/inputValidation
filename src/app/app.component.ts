e3b6db1777b2729eb9a13a91d4e39ce24dbc45ce
NandhiniNavaneethan251 <69723797+NandhiniNavaneethan251@users.noreply.github.com>
2021-02-18 13:12:52 +0530

    Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inputValidation';
+
+  public in="";
+ public error="";
+ public Domain="";
+ public userid="";
+  
+
+  constructor(){ }
+
+  ngOnInit(){
+
+  }
+  onClick(){
+    if (this.in == null || this.in == "") {
+      
+      this.error= "Field cannot be empty";
+      this.Domain= "";
+      this.userid= "";
+    
+    }
+    else {
+      var n2 = this.in.includes("/");
+      if (n2 == false) {
+          this.error = "Invalid Entry";
+          this.Domain = "";
+          this.userid= "";
+      } else {
+          var ind = this.in.indexOf("/");
+          this.Domain = this.in.substring(0, ind);
+          this.userid = this.in.substring(ind + 1);
+          if (this.Domain.length < 1 || this.userid.length < 1) {
+              this.error= "Invalid Entry";
+              this.Domain = "";
+              this.userid= "";
+          } else {
+
+              this.error= "";
+
+          }
+
+      }
+
+  }
+  }
+
 }
